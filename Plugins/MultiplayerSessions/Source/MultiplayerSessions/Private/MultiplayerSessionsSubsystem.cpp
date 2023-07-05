72e84a0e13b14a9250051bc6b40e671bfbb4d7a0
moonabyss <d-v@ukr.net>
2023-07-05 21:57:06 +0300

    02.14. Session Interface Delegates

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionsSubsystem.cpp
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionsSubsystem.cpp
@@ -4,6 +4,11 @@
 #include "OnlineSubsystem.h"
 
 UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem()
+    : CreateSessionCompleteDelegate(FOnCreateSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnCreateSessionComplete))
+    , FindSessionsCompleteDelegate(FOnFindSessionsCompleteDelegate::CreateUObject(this, &ThisClass::OnFindSessionsComplete))
+    , JoinSessionCompleteDelegate(FOnJoinSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnJoinSessionComplete))
+    , DestroySessionCompleteDelegate(FOnDestroySessionCompleteDelegate::CreateUObject(this, &ThisClass::OnDestroySessionComplete))
+    , StartSessionCompleteDelegate(FOnStartSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnStartSessionComplete))
 {
     IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
     if (Subsystem)
@@ -11,3 +16,23 @@ UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem()
         SessionInterface = Subsystem->GetSessionInterface();
     }
 }
+
+void UMultiplayerSessionsSubsystem::CreateSession(int32 NumPublicConnections, FString MatchType) {}
+
+void UMultiplayerSessionsSubsystem::FindSessions(int32 MaxSearchResults) {}
+
+void UMultiplayerSessionsSubsystem::JoinSession(const FOnlineSessionSearchResult& SessionResult) {}
+
+void UMultiplayerSessionsSubsystem::DestroySession() {}
+
+void UMultiplayerSessionsSubsystem::StartSession() {}
+
+void UMultiplayerSessionsSubsystem::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful) {}
+
+void UMultiplayerSessionsSubsystem::OnFindSessionsComplete(bool bWasSuccessful) {}
+
+void UMultiplayerSessionsSubsystem::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result) {}
+
+void UMultiplayerSessionsSubsystem::OnDestroySessionComplete(FName SessionName, bool bWasSuccessful) {}
+
+void UMultiplayerSessionsSubsystem::OnStartSessionComplete(FName SessionName, bool bWasSuccessful) {}
--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionsSubsystem.h
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionsSubsystem.h
@@ -16,7 +16,45 @@ class MULTIPLAYERSESSIONS_API UMultiplayerSessionsSubsystem : public UGameInstan
 public:
     UMultiplayerSessionsSubsystem();
 
+    /**
+     * To handle session functionality. The Menu class will call this.
+     */
+    void CreateSession(int32 NumPublicConnections, FString MatchType);
+    void FindSessions(int32 MaxSearchResults);
+    void JoinSession(const FOnlineSessionSearchResult& SessionResult);
+    void DestroySession();
+    void StartSession();
+
 protected:
+    /**
+     * Internal callbacks for the delegates we'll add to the Online Session Interface delegate list.
+     * These don't need to be called outside this class.
+     */
+    void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+    void OnFindSessionsComplete(bool bWasSuccessful);
+    void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+    void OnDestroySessionComplete(FName SessionName, bool bWasSuccessful);
+    void OnStartSessionComplete(FName SessionName, bool bWasSuccessful);
+
 private:
     IOnlineSessionPtr SessionInterface;
+
+    /**
+     * To add to the Online Session Interface delegate list.
+     * We'll bind our MultiplayerSessionSubsystem internal callbacks to these.
+     */
+    FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+    FDelegateHandle CreateSessionCompleteDelegate_Handle;
+
+    FOnFindSessionsCompleteDelegate FindSessionsCompleteDelegate;
+    FDelegateHandle FindSessionsCompleteDelegate_Handle;
+
+    FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;
+    FDelegateHandle JoinSessionCompleteDelegate_Handle;
+
+    FOnDestroySessionCompleteDelegate DestroySessionCompleteDelegate;
+    FDelegateHandle DestroySessionCompleteDelegate_Handle;
+
+    FOnStartSessionCompleteDelegate StartSessionCompleteDelegate;
+    FDelegateHandle StartSessionCompleteDelegate_Handle;
 };
