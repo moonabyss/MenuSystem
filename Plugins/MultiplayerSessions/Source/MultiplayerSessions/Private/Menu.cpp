1f446e50ac88ab01a72f6fd22cb020084d77e5ad
moonabyss <d-v@ukr.net>
2023-07-05 23:54:20 +0300

    02.16. Accessing our Subsystem

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
@@ -1,20 +1,45 @@
 // Multiplayer Sessions plugin. All rights reserved
 
 #include "Menu.h"
+#include "Components/Button.h"
+#include "MultiplayerSessionsSubsystem.h"
 
-void UMenu::MenuSetup() 
+bool UMenu::Initialize()
+{
+    if (!Super::Initialize()) return false;
+
+    HostButton->OnClicked.AddDynamic(this, &ThisClass::HostButtonClicked);
+    JoinButton->OnClicked.AddDynamic(this, &ThisClass::JoinButtonClicked);
+
+    return true;
+}
+
+void UMenu::MenuSetup()
 {
     check(GetWorld());
+    check(GetGameInstance());
 
     AddToViewport();
     SetVisibility(ESlateVisibility::Visible);
     bIsFocusable = true;
 
-    if (APlayerController* PC = GetWorld()->GetFirstPlayerController()){
+    if (APlayerController* PC = GetWorld()->GetFirstPlayerController())
+    {
         FInputModeUIOnly InputModeData;
         InputModeData.SetWidgetToFocus(TakeWidget());
         InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
         PC->SetInputMode(InputModeData);
         PC->SetShowMouseCursor(true);
     }
+
+    MultiplayerSessionsSubsystem = GetGameInstance()->GetSubsystem<UMultiplayerSessionsSubsystem>();
 }
+
+void UMenu::HostButtonClicked()
+{
+    if (!MultiplayerSessionsSubsystem) return;
+
+    MultiplayerSessionsSubsystem->CreateSession(4, FString("FreeForAll"));
+}
+
+void UMenu::JoinButtonClicked() {}
--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/Menu.h
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/Menu.h
@@ -4,7 +4,11 @@
 
 #include "Blueprint/UserWidget.h"
 #include "CoreMinimal.h"
+
 #include "Menu.generated.h"
+
+class UButton;
+class UMultiplayerSessionsSubsystem;
 
 UCLASS()
 class MULTIPLAYERSESSIONS_API UMenu : public UUserWidget
@@ -14,4 +18,23 @@ class MULTIPLAYERSESSIONS_API UMenu : public UUserWidget
 public:
     UFUNCTION(BlueprintCallable)
     void MenuSetup();
+
+protected:
+    bool Initialize() override;
+
+private:
+    UPROPERTY(meta = (BindWidget))
+    UButton* HostButton;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* JoinButton;
+
+    UFUNCTION()
+    void HostButtonClicked();
+
+    UFUNCTION()
+    void JoinButtonClicked();
+
+    // The subsystem designed to handle all online session functionality
+    UMultiplayerSessionsSubsystem* MultiplayerSessionsSubsystem;
 };
